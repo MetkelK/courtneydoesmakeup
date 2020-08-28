fc8345e5f22b1fa4ab4deac8963c5818e20e424f
Metkel <MetkelK@gmail.com>
2020-08-28 01:20:19 -0400

    refactored functions

--- Assets/Js/Main.js
+++ Assets/Js/Main.js
@@ -1,74 +1,104 @@
-var menuList = document.getElementById("menu-list");
-	
-var menuListOpen = 'menu-list-open';
+const documentApp = {};
 
-var handleClick = (e) => {
-	if (document.documentElement.clientWidth < 737) {
-		var offClick = (evt) => {
-			if (e !== evt) {
-				menuList.classList.toggle(menuListOpen);
-				document.removeEventListener('click', offClick, {passive: true})
+// let menuList = document.getElementById("menu-list");	
+// let menuListOpen = 'menu-list-open';
+// let handleClick = (e) => {
+// 	if (document.documentElement.clientWidth < 737) {
+// 		let offClick = (evt) => {
+// 			if (e !== evt) {
+// 				menuList.classList.toggle(menuListOpen);
+// 				document.removeEventListener('click', offClick, {passive: true})
+// 			}
+// 		}
+// 			if (!menuList.classList.contains(menuListOpen)) {
+// 				menuList.classList.toggle(menuListOpen);
+// 				document.addEventListener('click', offClick, {passive: true})
+// 			}
+// 		}
+// }
+
+// document.getElementById("menu").addEventListener("click", handleClick, {passive: true});
+
+// function that closes the menu when the user clicks off it
+documentApp.handleMenuClose = function () {
+	$('#menu').on('click', function(e) {
+		if ($(document).width() < 737) {
+			let offClick = function (event) {
+				if (e.target !== event.target) {
+					$('#menu-list').removeClass('menu-list-open');
+					documentApp.handleMenuClose();
+				}
+			}
+			if (!$('#menu-list').hasClass('menu-list-open')) {
+				$('#menu-list').addClass('menu-list-open');
+				$(document).on('click', offClick);
 			}
 		}
-			if (!menuList.classList.contains(menuListOpen)) {
-				menuList.classList.toggle(menuListOpen);
-				document.addEventListener('click', offClick, {passive: true})
+	})
+}
+
+// function that makes the navbar disappearing when the user scrolls down and reappear when the user scrolls up
+documentApp.disappearingNav = function () {
+	let prevScrollpos = $(window).scrollTop();
+	$(window).scroll(function() {
+		if ($(document).width() < 737) {
+			let currentScrollPos = $(window).scrollTop();
+			if (prevScrollpos < currentScrollPos) {
+				$('nav').addClass('nav-slide');
+			} else {
+				$('nav').removeClass('nav-slide');
 			}
+			prevScrollpos = currentScrollPos;
 		}
+	})
 }
 
-document.getElementById("menu").addEventListener("click", handleClick, {passive: true});
-
-
-// Disappearing Nav
-var nav = document.getElementById("navbar");
-var prevScrollpos = window.pageYOffset;
-
-function disappearingNav() {
-	if (document.documentElement.clientWidth < 737) {
-
-		var currentScrollPos = window.pageYOffset;
-		if (prevScrollpos < currentScrollPos) {
-			nav.classList.add('nav-slide');
-		} else {
-			nav.classList.remove('nav-slide');
-		}
-		prevScrollpos = currentScrollPos;
-	}
+//function that controls the lightbox gallery
+documentApp.lightboxGallery = function () {
+	$('#gallery').poptrox({
+	  	usePopupEasyClose: true,
+	    popupBackgroundColor: '#FFF5F8',
+	    usePopupForceClose: true
+	});
 }
 
-window.addEventListener('scroll', disappearingNav, {passive: true});
+//function that smooth scrolls to the gallery when the user click on the check me out button
+documentApp.scrollToGallery = function () {
+	$('.banner-button a').on('click', function(e) {
+		if (this.hash !== '') {
+			e.preventDefault();
+			const hash = this.hash;
+			$('html, body')
+	     .animate({
+	       scrollTop: $(hash).offset().top
+	       },800);
+		}
+	})
+}
 
-$(function() {
-  $('#gallery').poptrox({
-  	usePopupEasyClose: true,
-    popupBackgroundColor: '#FFF5F8'
-  });
+//function that smooth scrolls when the user clicks on the navbar buttons
+documentApp.smoothScroll = function () {
+	$('#menu-list a').on('click', function(e) {
+		if (this.hash !== '') {
+			e.preventDefault();
+			const hash = this.hash;
+			$('html, body')
+	     .animate({
+	       scrollTop: $(hash).offset().top
+	       },800);
+		}
+	})
+}
+
+documentApp.init = function () {
+	documentApp.lightboxGallery();
+	documentApp.scrollToGallery();
+	documentApp.smoothScroll();
+	documentApp.disappearingNav();
+	documentApp.handleMenuClose();
+}
+
+
+$(document).ready(function () {
+	documentApp.init();
 });
-
-
-$('#menu-list a').on('click', function(e) {
-	if (this.hash !== '') {
-		e.preventDefault();
-
-		const hash = this.hash;
-
-		$('html, body')
-     .animate({
-       scrollTop: $(hash).offset().top
-       },800);
-	}
-})
-
-$('.banner-button a').on('click', function(e) {
-	if (this.hash !== '') {
-		e.preventDefault();
-
-		const hash = this.hash;
-
-		$('html, body')
-     .animate({
-       scrollTop: $(hash).offset().top
-       },800);
-	}
-})
